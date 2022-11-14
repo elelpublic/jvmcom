e6cec052d2c9f977b3e10b20315f629b42a961ee
Lars Löffler <elelpublic@gmail.com>
2022-11-14 12:42:26 +0100

    Add boiler plate for integration tests.

---
+++ src/integration/java/com/infodesire/jvmcom/ClientServerTest.java
@@ -0,0 +1,17 @@
+package com.infodesire.jvmcom;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class ClientServerTest {
+
+  @Test
+  public void testCommunication() {
+
+    //Server server = new Server(  );
+    assertEquals( 1, 1 );
+
+  }
+
+}
