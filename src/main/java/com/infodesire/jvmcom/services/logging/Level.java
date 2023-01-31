dc439f384795f8c8b6ebb9b89334e4a413080fda
Lars Löffler <elelpublic@gmail.com>
2023-01-31 15:58:18 +0100

    Transform into slf4j levels.

--- src/main/java/com/infodesire/jvmcom/services/logging/Level.java
+++ src/main/java/com/infodesire/jvmcom/services/logging/Level.java
@@ -5,20 +5,20 @@ package com.infodesire.jvmcom.services.logging;
  */
 public enum Level {
 
-  OFF( 0 ), //	0	No logging
-  FATAL( 100 ), //	100	The application is unusable. Action needs to be taken immediately.
-  ERROR( 200 ), //	200	An error occurred in the application.
-  WARN( 300 ), //	300	Something unexpected—though not necessarily an error—happened and needs to be watched.
-  INFO( 400 ), //	400	A normal, expected, relevant event happened.
-  DEBUG( 500 ), //	500	Used for debugging purposes
-  TRACE( 600 )	// 600	Used for debugging purposes—includes the most detailed information
+  ERROR( 200, org.slf4j.event.Level.ERROR ), //	200	An error occurred in the application.
+  WARN( 300, org.slf4j.event.Level.WARN ), //	300	Something unexpected—though not necessarily an error—happened and needs to be watched.
+  INFO( 400, org.slf4j.event.Level.INFO ), //	400	A normal, expected, relevant event happened.
+  DEBUG( 500, org.slf4j.event.Level.DEBUG ), //	500	Used for debugging purposes
+  TRACE( 600, org.slf4j.event.Level.TRACE )	// 600	Used for debugging purposes—includes the most detailed information
 
   ;
 
   private final int numeric;
+  private org.slf4j.event.Level slf4jLevel;
 
-  Level( int numeric ) {
+  Level( int numeric, org.slf4j.event.Level slf4jLevel ) {
     this.numeric = numeric;
+    this.slf4jLevel = slf4jLevel;
   }
 
   /**
@@ -30,6 +30,10 @@ public enum Level {
    */
   public boolean isAtLeast( Level limit ) {
     return numeric <= limit.numeric;
+  }
+
+  public org.slf4j.event.Level asSlf4jLevel() {
+    return slf4jLevel;
   }
 
 }
